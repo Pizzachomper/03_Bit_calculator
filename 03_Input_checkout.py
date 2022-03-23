8116195756cdb2403f7c60bf27607be24c763823
Pizzachomper <100880007+Pizzachomper@users.noreply.github.com>
2022-03-24 11:47:48 +1300

    Add files via upload

---
+++ 03_Input_checkout.py
@@ -0,0 +1,17 @@
+# checks user choice is 'integer' 'text' or 'image'\
+def user_choice():
+
+    valid = False
+    while not valid:
+
+        response = "File type (integer / text / image): ".lower()
+
+        if response == "text" or response == "t":
+            return response
+
+        else:
+            print("Please choose a valid file type!")
+            print()
+
+# Main routine goes here
+data_type = user_choice()
